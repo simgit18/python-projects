710535b75d024a8b00adb139b9ef3e842ed13a56
simgit18 <shivamp1804@gmail.com>
2022-06-15 00:06:20 +0530

    minor improvements

--- Application using python/main.py
+++ Application using python/main.py
@@ -1,12 +1,37 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
+from datetime import datetime
+import json
 
 Builder.load_file("design.kv")
 
 
 class LoginScreen(Screen):
-    pass
+
+    def sign_up(self):
+        self.manager.current = "Signup_screen"
+
+
+class SignUp_Screen(Screen):
+    def add_user(self, uname, pword):
+        with open("users.json") as file:
+            users = json.load(file)
+            users[uname] = {"username": uname,
+                            "password": pword, "created": datetime.now().strftime("%Y-%m-%d %H-%M-%S")}
+
+            print(users)
+
+        with open("users.json", 'w') as file:
+            json.dump(users, file)
+
+        self.manager.current = "backTologin"
+
+
+class ContinueLogin(Screen):
+    def backTologin(self):
+        self.manager.transition.direction = 'right'
+        self.manager.current = "Login_screen"
 
 
 class RootWidget(ScreenManager):
